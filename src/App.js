5261fe0e92ac0cfec7b6a493d275496f4c0b48e5
zarashah14 <zarashahhussain22@gmail.com>
2024-01-26 16:04:53 +0500

    updatefirst

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import Logo from './assets/Logo';
+import Image from './assets/image'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+      <div className='h-screen items-center bg-blue-50 flex'>
+   <div className='flex flex-row mx-auto'>
+      <div className='flex flex-col'>
+        <div><Logo/></div>
+        <h3 className='text-DS-black font-source-sans-pro text-30 pt-10'>Simple</h3>
+        <p className='text-DS-black font-source-sans-pro text-3xl font-extrabold '>Design by</p>
+        <p className='text-DS-black font-source-sans-pro text-3xl font-extrabold '>Zara Shah</p>
+      </div>
+        <div className='flex flex-row'><Image/></div>
+        </div>
+        </div>
+      </>
   );
 }
 
