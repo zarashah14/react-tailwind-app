2f948b9249fe429dabf426183c4fc8c7ed796935
PirAdnan1 <109432989+PirAdnan1@users.noreply.github.com>
2024-01-27 22:55:34 -0800

    fix responsiveness

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
+import "./App.css";
+import Logo from "./assets/Logo";
+import Image from "./assets/image";
 
-import './App.css';
-import Logo from './assets/Logo';
-import Image from './assets/image'
+// components
+import Container from "./Components/Container";
 function App() {
   return (
-      <>
-      <div className='h-screen items-cente flex bg-purple-100'>
+    <>
+      {/* <div className='felx '>
       <div className='flex flex-row mx-auto'>
       <div className='flex flex-col'>
         <div className='mt-8'><Logo/></div>
@@ -17,10 +19,33 @@ function App() {
         </button>
 
       </div>
+        </div>
         <div className='flex flex-row'><Image/></div>
+        </div> */}
+      <Container>
+        <div className="flex flex-col lg:flex-row lg:justify-between items-center lg:px-12">
+          <div>
+            <Logo />
+            <h3 className="text-DS-black font-source-sans-pro text-30 pt-10">
+              Simple
+            </h3>
+            <p className="text-DS-Black font-source-sans-pro text-3xl font-extrabold ">
+              Design by
+            </p>
+            <p className="text-DS-black font-source-sans-pro text-3xl font-extrabold ">
+              Zara Shah
+            </p>
+            <button
+              type="button"
+              class="bg-gradient-to-r px-4 from-green-400 to-blue-500 transition hover:-translate-y-2  hover:from-pink-500 hover:to-yellow-500 py-4 rounded mt-6"
+            >
+              Follow US
+            </button>
+          </div>
+          <Image  />
         </div>
-        </div>
-      </>
+      </Container>
+    </>
   );
 }
 
---
+++ src/Components/Container.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Container({children}) {
+  return (
+    <div className='mx-auto max-w-[1460px]'>{children}</div>
+  )
+}
+
+export default Container
