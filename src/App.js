294a6a5f7db1285c0e8b00e3baf6bc83d23f18a8
zarashah14 <zarashahhussain22@gmail.com>
2024-03-03 11:29:58 +0500

    update3

--- src/App.js
+++ src/App.js
@@ -33,11 +33,11 @@ function App() {
         <div className="flex flex-col lg:flex-row lg:justify-between items-center lg:px-12">
           <div>
             <Logo />
-            <h3 className="text-DS-black font-source-sans-pro text-30 pt-10">
-              Simple
+            <h3 className="text-DS-Black font-source-sans-pro text-3xl font-extrabold pt-10">
+              Open
             </h3>
             <p className="text-DS-Black font-source-sans-pro text-3xl font-extrabold ">
-              Design by
+              COFFEE SHOP
             </p>
             <p className="text-DS-black font-source-sans-pro text-3xl font-extrabold ">
               Zara Shah
@@ -45,7 +45,7 @@ function App() {
             <button
               type="button"
               class="bg-gradient-to-r px-4 from-green-400 to-blue-500 transition hover:-translate-y-2  hover:from-pink-500 hover:to-yellow-500 py-4 rounded mt-6"
-            >
+            >  
               Follow US
             </button>
           </div>
--- src/Components/form.jsx
+++ src/Components/form.jsx
@@ -1,20 +1,43 @@
 import React from 'react'
-
+import Girl from '../assets/girl.png'
 function form() {
   return (
        <>
-            <h1>WELLCOME TO BRANKO</h1>
+         <div className='grid grid-cols-2 mx-10'>
+          <div className='mx-10'>
+            <h1  className='text-DS-Black font-source-sans-pro text-3xl font-extrabold'>WELLCOME TO BRANKO</h1>
+            <p className=' font-source-sans-pro'>Lorem ipsum, dolor sit amet consectetur
+             adipisicing elit. Sunt, ullam odio eveniet
+             delectus beatae maxime ipsum voluptatum et
+              haru soluta at quo tempore! Iste quidem 
+              iusto,magnam inventore nisi ab.</p>
+              <img src={Girl} alt="Description of your image" />
+              </div>
+
+             <div className='bg-gray-300 rounded-sm px-20 py-8 mx-40 mb-20'>
+            <h1 className='text-DS-Black font-source-sans-pro text-3xl font-extrabold'>Login Here</h1>
             <form className='my-6'>
-            <input type="tel" id="phone" name="phone" placeholder="Username" className='py-2'/><br></br>
-            <input type="tel" id="phone" name="phone" placeholder="Password" className='py-2 my-2'/><br></br>
+
+              <label>Full Name</label>
+            <input type="text" placeholder="Name" className='py-2 rounded'/><br></br>
+
+            <label>Email</label>
+            <input type="email" placeholder="Email address" className='py-2 my-2 rounded'/><br></br>
+
+            <label>UserName</label>
+            <input type="Username"  placeholder="Username" className='py-2 my-2 rounded'/><br></br>
+             
+            <label>Password</label>
+            <input type="password" id="pwd" name="pwd" placeholder="Password" className='py-2 my-2 rounded'/><br></br>
             <button
               type="button"
-              class="bg-gradient-to-r px-4 from-green-400 to-blue-500 transition hover:-translate-y-2  hover:from-pink-500 hover:to-yellow-500 py-4 rounded my-2"
+              class="bg-gradient-to-r px-4 from-green-400 to-blue-500 transition hover:-translate-y-2  hover:from-pink-500 hover:to-yellow-500 py-4 rounded my-2 mx-14"
             >
               Login
-            </button>
-            </form>
-   
+            </button>  
+            </form> 
+            </div>
+            </div>
        </>
   )
 }
