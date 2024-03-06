6241dd406c51c5bbe922c6ae1357d6a14613994e
zarashah14 <zarashahhussain22@gmail.com>
2024-03-06 13:28:40 +0500

    wednesdays

--- src/Components/form.jsx
+++ src/Components/form.jsx
@@ -14,31 +14,31 @@ function form() {
               <img src={Girl} className="" alt="Description of your image" />
               </div>
 
-             <div className='bg-gray-300 rounded-sm lg:px-20 lg:py-8 lg:mx-40 lg:mb-20 '>
-            <h1 className='text-DS-Black font-source-sans-pro text-3xl font-extrabold md:text-2xl '>Login Here</h1>
-            <form className='my-6'>
+             <div className='bg-gray-800 rounded-xl lg:px-20 lg:py-8 lg:mx-28 lg:my-20'>
+            <h1 className='text-white text-3xl font-bold md:text-2xl text-center p-4'>Register</h1>
+            <form className='flex flex-col justify-center items-center'>
 
-              <label>Full Name</label>
-            <input type="text" placeholder="Name" className='lg:py-2 rounded'/><br></br>
+            <label className='text-white'>Full Name</label>
+            <input type="text" placeholder="Name" className='rounded py-2'/><br/>
 
-            <label>Email</label>
-            <input type="email" placeholder="Email address" className='lg:py-2 my-2 rounded'/><br></br>
+            <label className='text-white'>Email</label>
+            <input type="email" placeholder="Email address" className='rounded py-2'/><br/>
 
-            <label>UserName</label>
-            <input type="Username"  placeholder="Username" className='lg:py-2 my-2 rounded'/><br></br>
+            <label className='text-white'>UserName</label>
+            <input type="Username"  placeholder="Username" className='rounded py-2'/><br/>
              
-            <label>Password</label>
-            <input type="password" id="pwd" name="pwd" placeholder="Password" className='lg:py-2 my-2 rounded'/><br></br>
+            <label className='text-white'>Password</label>
+            <input type="password" id="pwd" name="pwd" placeholder="Password" className='rounded py-2'/><br/>
             
            
-            <input type="checkbox" id="vehicle1" name="user"/>
-            <label for="user">I agree to the terms of user.</label>
+            <input type="checkbox" id="vehicle1" name="user" className=''/>
+            <label for="user" className='text-white'>I agree to the terms of user.</label>
 
             <button
               type="button"
-              class="bg-gradient-to-r px-4 from-green-400 to-blue-500 transition hover:-translate-y-2  hover:from-pink-500 hover:to-yellow-500 py-4 rounded my-2 mx-14"
+              class="bg-gradient-to-r px-6  from-green-400 to-blue-500 transition hover:-translate-y-2  hover:from-pink-500 hover:to-yellow-500 py-4 rounded-xl my-2 mx-14"
             >
-              Login
+              Register
             </button>  
             </form> 
             </div>
