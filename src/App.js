e9f4ff260cb2607d76351544bbeab66b54637868
zarashah14 <zarashahhussain22@gmail.com>
2024-03-07 20:44:09 +0500

    updathursday

--- src/App.js
+++ src/App.js
@@ -12,30 +12,9 @@ import Footer from './Components/Footer'
 function App() {
   return (
     <>
-      
-      <Container>
         <Navigation/>
-        {/* <div className="flex flex-col lg:flex-row lg:justify-between items-center lg:px-12"> 
-          <div>
-            <Logo/>
-            <h3 className="text-DS-black font-source-sans-pro text-30 pt-10">
-              Simple
-            </h3>
-            <p className="text-DS-Black font-source-sans-pro text-3xl font-extrabold ">
-              Design by
-            </p>
-            <p className="text-DS-black font-source-sans-pro text-3xl font-extrabold ">
-              Zara Shah
-            </p>
-            <button
-              type="button"
-              class="bg-gradient-to-r px-4 from-green-400 to-blue-500 transition hover:-translate-y-2  hover:from-pink-500 hover:to-yellow-500 py-4 rounded mt-6"
-            >
-              Follow US
-            </button>
-          </div>
-          <img src={Image} alt="Description of your image" />
-        </div> */}
+      <Container>
+      
         <div className="flex flex-col lg:flex-row lg:justify-between items-center lg:px-12">
           <div>
             {/* <Logo /> */}
@@ -60,11 +39,13 @@ function App() {
         </div>
         <Form/>
         <Card />
+        </Container>
+        
         <Services/>
         <Slider/>
         <Footer/>
-      </Container>
-
+     
+      
     </>
   );
 }
