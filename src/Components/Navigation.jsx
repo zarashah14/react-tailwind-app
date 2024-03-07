8d9ee74f15d3bc298a37ec877c39a29eb3c53ff6
zarashah14 <zarashahhussain22@gmail.com>
2024-03-07 12:43:11 +0500

    thursday3

--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -4,13 +4,16 @@ import Vector1 from '../assets/Vector1'
 import Vector2 from '../assets/Vector2'
 import Vector3 from '../assets/Vector3'
 import Vector4 from '../assets/Vector4'
+import Icon8  from'../assets/icons8.png'
 function Navigation() {
   return (
     <div className='bg-gray-900'>
+        
       <nav>
-        <div className='flex justify-between items-center p-6'>
+        <div className='hidden md:block'>
+        <div className='flex justify-between items-center h-24 p-4'>
              <div>
-             <ul className='list-none flex gap-5 '>
+             <ul className='list-none flex lg:gap-5 md:gap-3 '>
                 <li className=' text-white'>Home</li>
                 <li className=' text-white'>About</li>
                 <li className='  text-white'>Services</li>
@@ -26,7 +29,7 @@ function Navigation() {
 
 
              <div>
-             <ul className='list-none flex gap-5 '>
+             <ul className='list-none flex lg:gap-5 md:gap-3 '>
                 <li className=' text-white'><Vector1/></li>
                 <li className=' text-white'><Vector2/></li>
                 <li className=' text-white'><Vector3/></li>
@@ -35,7 +38,46 @@ function Navigation() {
              </div>
 
         </div>
+        </div>
+
+        <div className='block md:hidden'>
+            <div className='flex items-center justify-between h-24 p-4'>
+                <div>
+                    <Logo/>
+                </div>
+
+                <div>
+                    <button><img src={Icon8} alt="My image"/></button>
+                </div>
+            </div>
+
+            <div className='hidden' id='mobileMenuContent'>
+
+                <div>
+                <ul className='list-none flex flex-col ml-4 space-x-2'>
+                <li className='text-white p-3'>Home</li>
+                <li className='text-white p-3'>About</li>
+                <li className='text-white p-3'>Services</li>
+                <li className='text-white p-3'>Register</li>
+                <li className='text-white p-3'>Contact us</li>
+             </ul>
+                </div>
+
+                <div>
+                <ul className='list-none flex ml-4 space-x-2'>
+                <li className='p-3'><Vector1/></li>
+                <li className='p-3'><Vector2/></li>
+                <li className='p-3'><Vector3/></li>
+                <li className='p-3'><Vector4/></li>
+                </ul>
+                </div>
+                
+            </div>
+        </div>
+
       </nav>
+      
+      
     </div>
   )
 }
